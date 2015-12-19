462904aae25175865e4b69983b5ae1784bc97fe5
christiansongruenberg <christianson.gruenberg@gmail.com>
2015-12-19 03:06:19 -0500

    Part 4: Routing

---
+++ app/controllers/home.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Christianson
+ * Date: 19/12/2015
+ * Time: 2:23 AM
+ */
+
+class Home extends Controller
+{
+    public function index(){
+        echo 'home/index';
+    }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -8,7 +8,23 @@
 
 class App
 {
-    public function __construct(){
-        echo 'OK!';
+    //default controller and method
+    protected $controller = 'home';
+    protected $method = 'index';
+
+    //init the parameters from the url
+    protected $params = [];
+
+    public function __construct()
+    {
+        $url = $this->parseUrl();
+    }
+
+    //exploding and trimming the sanitized URL, will provide the controller, the method, and the params after that
+    public function parseUrl()
+    {
+        if(isset($_GET['url'])){
+            return $url = explode('/',filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
     }
 }
