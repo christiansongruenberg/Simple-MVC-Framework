6cfe692524807dae7a176aedcea0259dfbe398d4
christiansongruenberg <christianson.gruenberg@gmail.com>
2015-12-19 20:24:43 -0500

    Part 7: views

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -10,6 +10,9 @@ class Home extends Controller
 {
     public function index($name = '')
     {
-        $this->model('User');
+        $user = $this->model('User');
+        $user->name = $name;
+
+        $this->view('home/index', ['name' => $user->name]);
     }
 }
---
+++ app/views/home/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Christianson
+ * Date: 19/12/2015
+ * Time: 8:18 PM
+ */
+
+echo "Hello {$data['name']}";
